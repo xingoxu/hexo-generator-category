612be7119fb2fa899c7cad96269d3fa56dc41335
xingoxu <xingoxu@gmail.com>
2017-08-26 22:13:42 +0800

    add i18n
    fix perpage
    
    fix pagidir

--- index.js
+++ index.js
@@ -5,7 +5,8 @@
 var assign = require('object-assign');
 
 hexo.config.category_generator = assign({
-  per_page: typeof hexo.config.per_page === 'undefined' ? 10 : hexo.config.per_page
+  per_page: typeof hexo.config.per_page === 'undefined' ? 10 : hexo.config.per_page,
+  single_language: false
 }, hexo.config.category_generator);
 
 hexo.extend.generator.register('category', require('./lib/generator'));
